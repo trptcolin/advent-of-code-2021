3d65a6c75d98bbb7ea05e45536041ff861af496b
Colin Jones <trptcolin@gmail.com>
2021-12-02 18:08:41 -0600

    Format things consistently
    Using the Black formatter

--- 01/solution.py
+++ 01/solution.py
@@ -1,4 +1,5 @@
-from itertools import tee
+import itertools
+
 
 def read_ints(path):
     with open(path) as f:
@@ -8,37 +9,43 @@ def read_ints(path):
             except:
                 pass
 
+
 # available directly in itertools in python 3.10+
 def pairwise(xs):
-    a, b = tee(xs)
+    a, b = itertools.tee(xs)
     next(b, None)
     return zip(a, b)
 
+
 def count_increases(values):
     pairs = pairwise(values)
-    return len([(x, y) for (x,y) in pairs if x < y])
+    return len([(x, y) for (x, y) in pairs if x < y])
 
 
 def part_one(path):
     values = read_ints(path)
     return count_increases(values)
 
-print("Part 1:", part_one("./input.txt"))
 
 def triplewise(xs):
-    a, b, c = tee(xs, 3)
+    a, b, c = itertools.tee(xs, 3)
     next(b, None)
     next(c, None)
     next(c, None)
     return zip(a, b, c)
 
+
 def triple_sums(xs):
     triples = triplewise(xs)
-    return (a+b+c for (a,b,c) in triples)
+    return (a + b + c for (a, b, c) in triples)
+
 
 def part_two(path):
     values = read_ints(path)
     sums = triple_sums(values)
     return count_increases(sums)
 
-print("Part 2:", part_two("./input.txt"))
+
+if __name__ == "__main__":
+    print("Part 1:", part_one("./input.txt"))
+    print("Part 2:", part_two("./input.txt"))
--- 01/test_solution.py
+++ 01/test_solution.py
@@ -1,6 +1,7 @@
 import solution
 import tempfile
 
+
 def test_read_ints():
     with tempfile.NamedTemporaryFile() as f:
         f.write(b"1\n2\n3\n4\n5")
@@ -15,33 +16,37 @@ def test_read_ints():
         xs = solution.read_ints(f.name)
         assert list(xs) == [1, 2, 4, 5]
 
+
 def test_pairwise():
     assert list(solution.pairwise([])) == []
     assert list(solution.pairwise([1])) == []
-    assert list(solution.pairwise([1,2])) == [(1,2)]
-    assert list(solution.pairwise([1,2,3])) == [(1,2),(2,3)]
-    assert list(solution.pairwise(range(5))) == [(0,1), (1,2), (2,3), (3,4)]
+    assert list(solution.pairwise([1, 2])) == [(1, 2)]
+    assert list(solution.pairwise([1, 2, 3])) == [(1, 2), (2, 3)]
+    assert list(solution.pairwise(range(5))) == [(0, 1), (1, 2), (2, 3), (3, 4)]
+
 
 def test_triplewise():
     assert list(solution.triplewise([])) == []
     assert list(solution.triplewise([1])) == []
-    assert list(solution.triplewise([1,2])) == []
-    assert list(solution.triplewise([1,2,3])) == [(1,2,3)]
-    assert list(solution.triplewise(range(5))) == [(0,1,2), (1,2,3), (2,3,4)]
+    assert list(solution.triplewise([1, 2])) == []
+    assert list(solution.triplewise([1, 2, 3])) == [(1, 2, 3)]
+    assert list(solution.triplewise(range(5))) == [(0, 1, 2), (1, 2, 3), (2, 3, 4)]
+
 
 def test_count_increases():
     assert solution.count_increases([]) == 0
     assert solution.count_increases([0]) == 0
-    assert solution.count_increases([1,0]) == 0
-    assert solution.count_increases([1,1]) == 0
-    assert solution.count_increases([0,1]) == 1
-    assert solution.count_increases([0,1,2]) == 2
-    assert solution.count_increases([0,2,1,2]) == 2
-    assert solution.count_increases([0,3,1,2]) == 2
+    assert solution.count_increases([1, 0]) == 0
+    assert solution.count_increases([1, 1]) == 0
+    assert solution.count_increases([0, 1]) == 1
+    assert solution.count_increases([0, 1, 2]) == 2
+    assert solution.count_increases([0, 2, 1, 2]) == 2
+    assert solution.count_increases([0, 3, 1, 2]) == 2
+
 
 def test_triple_sums():
     assert list(solution.triple_sums([])) == []
     assert list(solution.triple_sums([1])) == []
-    assert list(solution.triple_sums([1,2])) == []
-    assert list(solution.triple_sums([1,2,3])) == [6]
-    assert list(solution.triple_sums([1,2,3,4])) == [6, 9]
+    assert list(solution.triple_sums([1, 2])) == []
+    assert list(solution.triple_sums([1, 2, 3])) == [6]
+    assert list(solution.triple_sums([1, 2, 3, 4])) == [6, 9]
