7da59639cff2e8d94646daae2f7d627592f527f5
Colin Jones <trptcolin@gmail.com>
2021-12-07 18:52:21 -0600

    Complete Day 7 Part 2
    Have to say I don't intuit why total cost has a reliable minimum in the
    nonlinear case. Taking the W for now.

--- 07/solution.py
+++ 07/solution.py
@@ -7,30 +7,48 @@ def read_positions(path):
         return [int(x) for x in line.split(",")]
 
 
+def distance(x, y):
+    return abs(x - y)
+
+
 def cost_to_align(xs, x0):
     cost = 0
     for x in xs:
-        cost += abs(x - x0)
+        cost += distance(x, x0)
     return cost
 
 
-def part_one(path):
+def sum_1_to_n(n):
+    return int(n * (n + 1) / 2)
+
+
+def nonlinear_cost_to_align(xs, x0):
+    return sum([sum_1_to_n(distance(x, x0)) for x in xs])
+
+
+def find_best_cost(positions, cost_function):
     # Idea: start searching at the bottom, and as soon as the cost increases
     # again, you're out of the local minimum (which is also a global minimum
-    # since it's 1-dimensional). Binary search came to mind but you risk
-    # leaping over the minimum and not being able to interpret changes.
-    positions = read_positions(path)
+    # since it's 1-dimensional - feels more obvious for linear than nonlinear
+    # cost functions...). Binary search came to mind but you risk leaping over
+    # the minimum and not being able to interpret changes.
     best_so_far = math.inf
     for n in range(0, max(positions) + 1):
-        this_cost = cost_to_align(positions, n)
+        this_cost = cost_function(positions, n)
         if this_cost < best_so_far:
             best_so_far = this_cost
         else:
             return best_so_far
 
 
+def part_one(path):
+    positions = read_positions(path)
+    return find_best_cost(positions, cost_to_align)
+
+
 def part_two(path):
-    pass
+    positions = read_positions(path)
+    return find_best_cost(positions, nonlinear_cost_to_align)
 
 
 if __name__ == "__main__":
--- 07/test_solution.py
+++ 07/test_solution.py
@@ -45,3 +45,33 @@ def test_part_one():
         f.seek(0)
 
         assert solution.part_one(f.name) == 37
+
+
+def test_nonlinear_cost_to_align():
+    xs = [1, 2, 3]
+    assert solution.nonlinear_cost_to_align(xs, 0) == 1 + (1 + 2) + (1 + 2 + 3)
+
+
+def test_nonlinear_cost_to_align_from_example():
+    with tempfile.NamedTemporaryFile() as f:
+        f.write(bytes(example_input, "UTF-8"))
+        f.seek(0)
+
+        positions = solution.read_positions(f.name)
+        assert positions == [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
+        assert solution.nonlinear_cost_to_align(positions, 1) == 242
+        assert solution.nonlinear_cost_to_align(positions, 2) == 206
+        assert solution.nonlinear_cost_to_align(positions, 3) == 183
+        assert solution.nonlinear_cost_to_align(positions, 4) == 170
+        assert solution.nonlinear_cost_to_align(positions, 5) == 168
+        assert solution.nonlinear_cost_to_align(positions, 6) == 176
+        assert solution.nonlinear_cost_to_align(positions, 7) == 194
+        assert solution.nonlinear_cost_to_align(positions, 8) == 223
+
+
+def test_part_two():
+    with tempfile.NamedTemporaryFile() as f:
+        f.write(bytes(example_input, "UTF-8"))
+        f.seek(0)
+
+        assert solution.part_two(f.name) == 168
