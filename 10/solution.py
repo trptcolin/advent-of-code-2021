82bd093c248b3bb8904d34ade403dd60f18615be
Colin Jones <trptcolin@gmail.com>
2021-12-10 08:16:19 -0600

    Complete Day 10 part 2

--- 10/solution.py
+++ 10/solution.py
@@ -1,3 +1,5 @@
+import statistics
+
 matching_end_brace = {"[": "]", "{": "}", "(": ")", "<": ">"}
 
 end_braces = set(matching_end_brace.values())
@@ -19,6 +21,35 @@ class Error:
     def score(self):
         return self.scores[self.actual]
 
+    def __repr__(self):
+        return f"Error(expected={self.expected}, actual={self.actual})"
+
+
+class Complete:
+    def __init__(self, line):
+        self.line = line
+
+
+class Incomplete:
+    def __init__(self, line, chunk_stack):
+        self.line = line
+        self.chunk_stack = chunk_stack
+
+    def __eq__(self, other):
+        if not isinstance(other, Incomplete):
+            return False
+        return self.line == other.line and self.chunk_stack == other.chunk_stack
+
+    def __repr__(self):
+        return f"Incomplete(chunk_stack={self.chunk_stack})"
+
+    def autocomplete(self):
+        result = []
+        while len(self.chunk_stack) > 0:
+            c = self.chunk_stack.pop()
+            result.append(matching_end_brace[c])
+        return "".join(result)
+
 
 def find_error(line):
     chunk_stack = []
@@ -36,22 +67,37 @@ def find_error(line):
                 return Error(expected_end_brace, c)
             else:
                 chunk_stack.append(c)
+    if len(chunk_stack) == 0:
+        return Complete(line)
+    else:
+        return Incomplete(line, chunk_stack)
 
 
 def read_errors(path):
     with open(path) as f:
         for line in f:
-            e = find_error(line.strip())
-            if e != None:
-                yield e
+            yield find_error(line.strip())
+
+
+completion_char_scores = {")": 1, "]": 2, "}": 3, ">": 4}
+
+
+def completion_score(completion):
+    score = 0
+    for c in completion:
+        score *= 5
+        score += completion_char_scores[c]
+    return score
 
 
 def part_one(path):
-    return sum(e.score() for e in read_errors(path))
+    return sum(e.score() for e in read_errors(path) if isinstance(e, Error))
 
 
 def part_two(path):
-    pass
+    incomplete_lines = (e for e in read_errors(path) if isinstance(e, Incomplete))
+    scores = (completion_score(e.autocomplete()) for e in incomplete_lines)
+    return statistics.median(scores)
 
 
 if __name__ == "__main__":
--- 10/test_solution.py
+++ 10/test_solution.py
@@ -39,3 +39,23 @@ def test_find_error():
 
     e = solution.find_error("<{([([[(<>()){}]>(<<{{")
     assert e == solution.Error("]", ">")
+
+
+def test_completing_an_incomplete_line():
+    line = "[({(<(())[]>[[{[]{<()<>>"
+    e = solution.find_error(line)
+    assert e == solution.Incomplete(line, list("[({([[{{"))
+    assert e.autocomplete() == "}}]])})]"
+
+
+def test_completion_score():
+    assert solution.completion_score("])}>") == 294
+    assert solution.completion_score("}}>}>))))") == 1480781
+
+
+def test_part_two():
+    with tempfile.NamedTemporaryFile() as f:
+        f.write(bytes(example_input, "UTF-8"))
+        f.seek(0)
+
+        assert solution.part_two(f.name) == 288957
